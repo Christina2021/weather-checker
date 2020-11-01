97f8281c70d8801888552125075bf54b48726833
Christina2021 <65867599+Christina2021@users.noreply.github.com>
2020-10-31 20:41:53 -0700

    Added open weather icons to site.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,13 @@
 //Tested and linked
 
+//Prevent Enter Key from doing default
+$(window).keydown(function(event){
+    if(event.keyCode == 13) {
+      event.preventDefault();
+      return false;
+    }
+});
+
 var myKey = hideKey.apiKey;
 var cityName;
 var lat, lon;
@@ -24,16 +32,28 @@ function addSearch(e) {
     $('#city-buttons').prepend(newCityButton);
 
     $('#search-city').val('');
-//after creates button,, calls cityWeatherInformation
-    //cityWeatherInformation();
-//need to save button to local storage
 
+    cityButtonName = cityButtonName.toLowerCase().split(' ').join('+');
+    cityWeatherInformation(cityButtonName);
     
+    //need to save button to local storage
+
 }
 
 
-//Show results = city button clicked
-function cityWeatherInformation(event){
+//When city button clicked
+function searchHistoryCity(event){
+
+    //Add City
+    let cityWeatherData = '';
+    cityWeatherData = event.target.id;
+    cityWeatherData = cityWeatherData.toLowerCase().split(' ').join('+');
+    cityWeatherInformation(cityWeatherData);
+
+}
+
+//Show results
+function cityWeatherInformation(cityName){
 
     //Remove hidden
     $('.results').removeAttr('hidden');
@@ -45,13 +65,6 @@ function cityWeatherInformation(event){
         $(resetFiveDay).empty();
     };
 
-    //Add City
-    let cityWeatherData = '';
-    cityWeatherData = event.target.id;
-    cityWeatherData = cityWeatherData.toLowerCase().split(' ').join('+');
-    cityName = cityWeatherData;
-
-
     currentURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=" + myKey;
 
     //Current Forecast
@@ -60,36 +73,83 @@ function cityWeatherInformation(event){
         method: "GET"
     })
     .then(function(response1){
-        console.log("currentURL object" + response1);
+
         //Set lat and lon for all data
         lat = response1.coord.lat;
         lon = response1.coord.lon;
-        console.log(lat,lon);
         fiveDayURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon +"&exclude=minutely,hourly,alerts&units=imperial&appid=" + myKey;
 
         //name of city and date called
-        console.log(response1.name);
         $('.name').text(response1.name);
 
         let unixTime = response1.dt;
         let dateData = new Date(unixTime * 1000);
-        console.log(dateData);
         let month = dateData.getMonth() + 1;
         let date = dateData.getDate();
         let year = dateData.getFullYear();
-        console.log(month, date, year);
         
         $('#current-weather').append($('<h2>').addClass("mb-3").text(`${response1.name} (${month}/${date}/${year})`));
+        //Add icon to h2 based on current weather conditions
+        let currentWeatherCondition = response1.weather[0].description;
+        let currentWeatherConditionIcon = $('<img>')
+        let iconURL; 
 
+        switch(currentWeatherCondition){
+            case 'clear sky':
+                iconURL = "http://openweathermap.org/img/wn/01d@2x.png";
+                currentWeatherConditionIcon.attr('src', iconURL)
+                $('h2').append($('<span>').html(currentWeatherConditionIcon));
+                break;
+            case 'few clouds':
+                iconURL = "http://openweathermap.org/img/wn/02d@2x.png";
+                currentWeatherConditionIcon.attr('src', iconURL)
+                $('h2').append($('<span>').html(currentWeatherConditionIcon));
+                break;
+            case 'scattered clouds':
+                iconURL = "http://openweathermap.org/img/wn/03d@2x.png";
+                currentWeatherConditionIcon.attr('src', iconURL)
+                $('h2').append($('<span>').html(currentWeatherConditionIcon));
+                break;
+            case 'broken clouds':
+                iconURL = "http://openweathermap.org/img/wn/04d@2x.png";
+                currentWeatherConditionIcon.attr('src', iconURL)
+                $('h2').append($('<span>').html(currentWeatherConditionIcon));
+                break;       
+            case 'shower rain':
+                iconURL = "http://openweathermap.org/img/wn/09d@2x.png";
+                currentWeatherConditionIcon.attr('src', iconURL)
+                $('h2').append($('<span>').html(currentWeatherConditionIcon));
+                break;
+            case 'rain':
+                iconURL = "http://openweathermap.org/img/wn/10d@2x.png";
+                currentWeatherConditionIcon.attr('src', iconURL)
+                $('h2').append($('<span>').html(currentWeatherConditionIcon));
+                break;      
+            case 'thunderstorm':
+                iconURL = "http://openweathermap.org/img/wn/11d@2x.png";
+                currentWeatherConditionIcon.attr('src', iconURL)
+                $('h2').append($('<span>').html(currentWeatherConditionIcon));
+                break;
+            case 'snow':
+                iconURL = "http://openweathermap.org/img/wn/13d@2x.png";
+                currentWeatherConditionIcon.attr('src', iconURL)
+                $('h2').append($('<span>').html(currentWeatherConditionIcon));
+                break;
+            case 'mist':
+                iconURL = "http://openweathermap.org/img/wn/50d@2x.png";
+                currentWeatherConditionIcon.attr('src', iconURL)
+                $('h2').append($('<span>').html(currentWeatherConditionIcon));
+                break;
+            default: 
+                console.log("Issue with the icons")      
+        };
 
-        //UV in and 5-Day Forecast
+        //Current and 5-Day Forecast
         $.ajax({
             url: fiveDayURL,
             method: "GET"
         })
         .then(function(response2){
-            console.log(fiveDayURL);
-            console.log(response2);
         
             // set current temperature
             $('#current-weather').append($('<p>').html(`Temperature: ${response2.current.temp} &#8457;`));
@@ -101,7 +161,11 @@ function cityWeatherInformation(event){
             $('#current-weather').append($('<p>').text(`Wind Speed: ${response2.current.wind_speed} MPH`));
 
             // set uv index
-            $('#current-weather').append($('<p>').text(`UV Index: ${response2.current.uvi}`));
+            let uvIndex = $('<p>');
+            $('#current-weather').append(uvIndex.text(`UV Index: `));
+            uvIndex.append($('<span>').text(`${response2.current.uvi}`));
+            //add id to span based on uv index
+
 
             // get 5 day forecast
             for (i = 1; i < 6; i++){
@@ -114,25 +178,70 @@ function cityWeatherInformation(event){
                 let dayMonth = newDateData.getMonth() + 1;
                 let dayDate = newDateData.getDate();
                 let dayYear = newDateData.getFullYear();
-                console.log(dayMonth, dayDate, dayYear);
 
-                //next temp
-                console.log((`Temp: ${day.temp.day} &#8457;`));
-
-                //next Humidity
-                console.log((`Humidity: ${day.humidity}%`));
 
 
                 //Append date
                 let addTo = '#day-' + i;
-                console.log(addTo);
 
                 $(addTo).append($('<h6>').text(`${dayMonth}/${dayDate}/${dayYear}`));
+
+                let fiveDayIcon = $('<img>');
+
+                //Add icon
+                switch(currentWeatherCondition){
+                    case 'clear sky':
+                        iconURL = "http://openweathermap.org/img/wn/01d@2x.png";
+                        fiveDayIcon.attr('src', iconURL)
+                        $(addTo).append(fiveDayIcon);
+                        break;
+                    case 'few clouds':
+                        iconURL = "http://openweathermap.org/img/wn/02d@2x.png";
+                        fiveDayIcon.attr('src', iconURL)
+                        $(addTo).append(fiveDayIcon);
+                        break;
+                    case 'scattered clouds':
+                        iconURL = "http://openweathermap.org/img/wn/03d@2x.png";
+                        fiveDayIcon.attr('src', iconURL)
+                        $(addTo).append(fiveDayIcon);
+                        break;
+                    case 'broken clouds':
+                        iconURL = "http://openweathermap.org/img/wn/04d@2x.png";
+                        fiveDayIcon.attr('src', iconURL)
+                        $(addTo).append(fiveDayIcon);
+                        break;       
+                    case 'shower rain':
+                        iconURL = "http://openweathermap.org/img/wn/09d@2x.png";
+                        fiveDayIcon.attr('src', iconURL)
+                        $(addTo).append(fiveDayIcon);
+                        break;
+                    case 'rain':
+                        iconURL = "http://openweathermap.org/img/wn/10d@2x.png";
+                        fiveDayIcon.attr('src', iconURL)
+                        $(addTo).append(fiveDayIcon);
+                        break;      
+                    case 'thunderstorm':
+                        iconURL = "http://openweathermap.org/img/wn/11d@2x.png";
+                        fiveDayIcon.attr('src', iconURL)
+                        $(addTo).append(fiveDayIcon);
+                        break;
+                    case 'snow':
+                        iconURL = "http://openweathermap.org/img/wn/13d@2x.png";
+                        fiveDayIcon.attr('src', iconURL)
+                        $(addTo).append(fiveDayIcon);
+                        break;
+                    case 'mist':
+                        iconURL = "http://openweathermap.org/img/wn/50d@2x.png";
+                        fiveDayIcon.attr('src', iconURL)
+                        $(addTo).append(fiveDayIcon);
+                        break;
+                    default: 
+                        console.log("Issue with the icons")      
+                };
 
                 $(addTo).append($('<p>').html(`Temp: ${day.temp.day} &#8457;`));
 
                 $(addTo).append($('<p>').text(`Humidity: ${day.humidity}%`));
-
 
 
             };
@@ -151,7 +260,7 @@ function cityWeatherInformation(event){
 $('#search-button').click(addSearch);
 
 //city button clicked
-$('#city-buttons').click(cityWeatherInformation);
+$('#city-buttons').click(searchHistoryCity);
 
 
 //call local storage function once page opens
@@ -159,7 +268,6 @@ $('#city-buttons').click(cityWeatherInformation);
 
 
 //to-do still
-//update city name from search feature
-//add icons for UV index, conditions (cloudy/sunny/rainy/etc.)
+//add background for UV index
 //add city buttons to local storage
 //get city buttons from local storage
